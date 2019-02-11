f3620efff04b8a5220d0a3f3c23c50cbb018de1f
Ewen4002169 <4002169@sps-student.sk.ca>
2019-02-11 10:56:48 -0600

    New Project
    Started project, February 11, 2019

--- cs30-p5js-template-master/sketch.js
+++ cs30-p5js-template-master/sketch.js
@@ -7,9 +7,27 @@
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(900, 900);
+  background(0,0,255);
 }
 
+function keyTyped() {
+  if (key === "a") {
+    rect(mouseX,mouseY,50,50);
+  }
+  if (key === "s") {
+    ellipse(mouseX, mouseY, 100, 100);
+  }
+  if (key === "d") {
+    triangle(mouseX,mouseY,mouseX/2,mouseY/2,mouseX*2,mouseY*2);
+  }
+  if (mouseIsPressed){
+    fill(random(0,255), random(0,255), random(0,255));
+  }
+}
+
+
 function draw() {
-  background(220);
+  textFont("Times New Roman");
+  text("Evan Ewen", 12, 30);
 }
