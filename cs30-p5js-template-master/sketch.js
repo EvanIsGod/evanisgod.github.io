ae04647d0a18b9a6b60570a0ddd8899c4accf120
Ewen4002169 <4002169@sps-student.sk.ca>
2019-02-12 15:48:56 -0600

    started random garbage

--- cs30-p5js-template-master/sketch.js
+++ cs30-p5js-template-master/sketch.js
@@ -7,9 +7,17 @@
 
 
 function setup() {
-  createCanvas(900, 900);
+  createCanvas(windowWidth, windowHeight);
   background(0,0,255);
 }
 
 function draw() {
+  fill(255,51,0);
+  ellipse(windowWidth/2, windowHeight/2, 90, 140);
+  fill(0,0,0);
+  ellipse(windowWidth/2 - 30, windowHeight/2 - 30, 10,5);
+  ellipse(windowWidth/2 + 30, windowHeight/2 - 30, 10,5);
+  line(windowWidth/2 - 20, windowHeight/2 + 10, windowWidth/2, windowHeight/2 + 20);
+  line(windowWidth/2 + 20, windowHeight/2 + 10, windowWidth/2, windowHeight/2 + 20);
+
 }
