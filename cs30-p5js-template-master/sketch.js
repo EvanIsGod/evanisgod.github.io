dd57c59c2048c55429837aba92187976dba8e503
Ewen4002169 <4002169@sps-student.sk.ca>
2019-02-13 10:20:59 -0600

    Update sketch.js

--- cs30-p5js-template-master/sketch.js
+++ cs30-p5js-template-master/sketch.js
@@ -8,16 +8,9 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0,0,255);
+  background(0);
 }
 
 function draw() {
-  fill(255,51,0);
-  ellipse(windowWidth/2, windowHeight/2, 90, 140);
-  fill(0,0,0);
-  ellipse(windowWidth/2 - 30, windowHeight/2 - 30, 10,5);
-  ellipse(windowWidth/2 + 30, windowHeight/2 - 30, 10,5);
-  line(windowWidth/2 - 20, windowHeight/2 + 10, windowWidth/2, windowHeight/2 + 20);
-  line(windowWidth/2 + 20, windowHeight/2 + 10, windowWidth/2, windowHeight/2 + 20);
-
+  
 }
