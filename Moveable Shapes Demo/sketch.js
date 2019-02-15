60f48d1b6332e73bed2147b66dee01a896af3257
Ewen4002169 <4002169@sps-student.sk.ca>
2019-02-15 10:37:30 -0600

    Ma;.de a pickup soqare

---
+++ Moveable Shapes Demo/sketch.js
@@ -0,0 +1,36 @@
+
+let x = 200;
+let y  = 200;
+let rSize = 100;
+let pickedUp = false;
+let mouseOver = false;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(0,255,0);
+  fill(255);
+  mouseOver = false;
+  if (mouseX >= x - rSize/2 && mouseX <= x + rSize/2 && mouseY >= y - rSize/2 && mouseY <= y + rSize/2){
+    fill(0);
+    mouseOver = true;
+  }
+  if (pickedUp){
+    x = mouseX;
+    y = mouseY;
+  }
+  rect(x,y,rSize,rSize);
+}
+
+function mousePressed() {
+  if (mouseOver === true){
+    pickedUp = true;
+  }
+}
+
+function mouseReleased(){
+  pickedUp = false;
+}
