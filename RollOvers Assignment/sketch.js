f9777e51b2c1f16910f4bf35ed4e7c00072f026a
Ewen4002169 <4002169@sps-student.sk.ca>
2019-03-04 10:59:53 -0600

    Broke Rollover Assignment Even More
    March 4, 2019

--- RollOvers Assignment/sketch.js
+++ RollOvers Assignment/sketch.js
@@ -18,45 +18,59 @@ function setup() {
 
 function draw() {
   stroke(0);
-  
-  mouseOver();
   fill(fadeColour);
   rect(rectX,rectY,rectSize,rectSize);
   fill(fadeColour2);
   rect(rectX+rectSize,rectY+rectSize, rectSize,rectSize);
-
+  fill(fadeColour3);
   rect(rectX+rectSize,rectY, rectSize,rectSize);
-
+  fill(fadeColour4);
   rect(rectX,rectY+rectSize, rectSize,rectSize);
+  mouseOver();
  
   fadeColour -= (1,1,1);
+  fadeColour2 -= (1,0,0);
+  fadeColour3 -= (0,1,0);
+  fadeColour4 -= (0,0,1);
   if (mouseIsPressed){
     fadeColour = (255,255,255);
+    fadeColour2 = (255,0,0);
+    fadeColour3 = (0,255,0);
+    fadeColour4 = (0,0,255);
   }
 }
 
 function mouseOver(){
   if (mouseX >= rectX && mouseX <= rectX+rectSize && mouseY >= rectY && mouseY <= rectY+rectSize){
-    fadeColour -= (1,1,1);
-    //rect(rectX,rectY, rectSize,rectSize);
+    fill(fadeColour);
+    rect(rectX,rectY, rectSize,rectSize);
     print("on");
     mouseMoved();
   }
   else if (mouseX >= rectX+rectSize && mouseX <= rectX+rectSize*2 && mouseY >= rectY+rectSize && mouseY <= rectY+rectSize*2){
-    fill(255,0,255);
+    fill(fadeColour2);
     rect(rectX+rectSize,rectY+rectSize, rectSize,rectSize);
+    mouseMoved2();
   }
   else if (mouseX >= rectX && mouseX <= rectX+rectSize && mouseY >= rectY+rectSize && mouseY <= rectY+rectSize+rectSize){
-    fill(0,255,0);
+    fill(fadeColour3);
     rect(rectX,rectY+rectSize, rectSize,rectSize);
   }
   else if (mouseX >= rectX+rectSize && mouseX <= rectX+rectSize+rectSize && mouseY >= rectY && mouseY <= rectY+rectSize){
-    fill(120,0,60);
+    fill(fadeColour4);
     rect(rectX+rectSize,rectY, rectSize,rectSize);
   }
 }
 
 function mouseMoved(){
+  if ((mouseX >= rectX && mouseX <= rectX+rectSize && mouseY >= rectY && mouseY <= rectY+rectSize) === false){
+    fill(fadeColour);
+    rect(rectX,rectY, rectSize,rectSize);
+    print("gone");
+  }
+}
+
+function mouseMoved2(){
   if ((mouseX >= rectX && mouseX <= rectX+rectSize && mouseY >= rectY && mouseY <= rectY+rectSize) === false){
     fill(fadeColour);
     rect(rectX,rectY, rectSize,rectSize);
