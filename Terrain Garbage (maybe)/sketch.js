a90ffc8c27c270ee595fa8b9a39476315650ea2d
Ewen4002169 <4002169@sps-student.sk.ca>
2019-03-14 11:02:01 -0600

    Updated Terrain Generation
    Messed it up even more

--- Terrain Garbage (maybe)/sketch.js
+++ Terrain Garbage (maybe)/sketch.js
@@ -3,19 +3,17 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
   fill(255);
-  //rectMode(CENTER);
-  //noLoop();
-  generateTerrain();
-}
-
-function generateTerrain(){
-  for (let i = 0; i < width; i += 5) {
-    rect(i, noise(yOff), 5, windowHeight);
-    fill(random(0,255),random(0,255),random(0,255));
-  }
+  rectMode(CENTER);
+  noLoop();
 }
 
 function draw() {
-  yOff += 0.01;
-  map(noise,0,1,0,height);
+  let f = map(noise(yOff),0,1,0,height*2);
+  for (let i = 0; i < width; i += 5) {
+    rect(i, height, 5, f);
+    rect(i, height, 5, f);
+    fill(random(0,255),random(0,255),random(0,255));
+    yOff += 0.01;
+  }
 }
+
